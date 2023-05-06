32ecb2457cb34b770f7b12a55f7bba4a099265a2
Jason Crockett <JesusisLord33333@outlook.com>
2023-05-06 07:24:05 +0100

    beta version

--- lykke_private_api.rb
+++ lykke_private_api.rb
@@ -8,7 +8,13 @@ module LykkeRestApi
     headers {"Authorization" => "Bearer #{API_KEY}"}
 
     def place_a_limit_order(assetPairId,side,volume,price)
-      JSON.parse(self.class.post('/orders/limit',{body:{"assetPairId" => assetPairId,"side" => side,"volume" => volume,"price" => price}}).body)
+      request = self.class.post('/orders/limit',{body:{assetPairId: assetPairId,side: side,volume: volume.to_f,price:price.to_f}})
+      begin
+        return JSON.parse(request.body)
+      rescue
+        puts request.response
+        exit
+      end
     end
     def get_order_by_id(order_id)
       JSON.parse(self.class.get("/orders/#{order_id.to_s}").body)
